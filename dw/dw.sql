d732db852a094b51e61080c103f9dfc4e5cdcdb0
zakpaw <zakpaw36@gmail.com>
2021-04-12 18:20:56 +0200

    DW & inserts

---
+++ dw/dw.sql
@@ -0,0 +1,111 @@
+-- DIMENTIONS --
+CREATE TABLE TravelAgency(
+	agency_ID INT PRIMARY KEY,
+	agency_name VARCHAR(100),
+	city VARCHAR(100),
+	country VARCHAR(100)
+);
+
+CREATE TABLE Employee(
+	employee_ID INT PRIMARY KEY,
+	travel_agency_ID INT REFERENCES TravelAgency,
+	PESEL VARCHAR(11),
+	name_surname VARCHAR(45),
+    education VARCHAR(14),
+	seniority VARCHAR(27),
+	is_active BIT -- tutaj mialo byc "active" albo "not active" ale nie wiem
+);
+ALTER TABLE Employee 
+ADD CONSTRAINT chk_education 
+	CHECK (education IN ('Middle school', 'High School', 'College', 'Master', 'PhD') 
+		AND seniority IN ('up to one year', 'between one and five years', 'more than five years'));
+
+CREATE TABLE Date(
+	date_ID INT PRIMARY KEY,
+	date INT CHECK(date BETWEEN 1 and 31),
+	year INT,
+	month VARCHAR(20),
+	monthNo INT CHECK(monthNo BETWEEN 1 and 12)
+);
+
+CREATE TABLE Time(
+	time_ID INT PRIMARY KEY, 
+	hour INT CHECK(hour BETWEEN 0 AND 24),
+	minute INT CHECK(minute BETWEEN 0 AND 60)
+);
+
+CREATE TABLE LocalAirport(
+	airport_ID INT PRIMARY KEY,
+	airport_name VARCHAR(150),
+	city VARCHAR(150),
+	country VARCHAR(150)
+);
+
+CREATE TABLE DestinationAirport(
+	airport_ID INT PRIMARY KEY,
+	airport_name VARCHAR(150),
+	city VARCHAR(150),
+	country VARCHAR(150)
+);
+
+CREATE TABLE Airline(
+	airport_ID INT PRIMARY KEY,
+	name VARCHAR(150)
+);
+
+CREATE TABLE Hotel(
+	hotel_ID INT PRIMARY KEY,
+	hotel_name VARCHAR(150),
+	city VARCHAR(150),
+	country VARCHAR(150),
+	is_befriended BIT DEFAULT 0
+);
+
+-- FACTS --
+CREATE TABLE AirportNearHotel(
+	hotel_ID INT REFERENCES Hotel,
+	airport_ID INT REFERENCES DestinationAirport,
+	distance INT,
+	travelled_time INT
+);
+
+CREATE TABLE Flights(
+	flight_ID INT PRIMARY KEY,
+	flight_NO INT,
+	airline_ID INT REFERENCES Airline,
+	local_airport_ID INT REFERENCES LocalAirport,
+	destination_airport_ID INT REFERENCES DestinationAirport,
+	flight_departure_start_ID INT REFERENCES Time,
+	flight_departure_end_ID INT REFERENCES Time,
+	flight_duration INT,
+	cost MONEY,
+	discount INT
+);
+
+CREATE TABLE TripScheduling(
+	employee_ID INT REFERENCES Employee,
+	hotel_ID INT REFERENCES Hotel,
+	creation_date_ID INT REFERENCES Date,
+	creation_time_ID INT REFERENCES Time,
+	hotel_start_date_ID INT REFERENCES Date,
+	hotel_end_date_ID INT REFERENCES Date,
+	hotel_start_time_ID INT REFERENCES Time,
+	hotel_end_time_ID INT REFERENCES Time,
+	transaction_NO INT,
+	hotel_cost MONEY,
+	client_price MONEY,
+	profit MONEY,
+	OTW INT
+);
+
+DROP TABLE TripScheduling;
+DROP TABLE Flights;
+DROP TABLE AirportNearHotel;
+DROP TABLE Employee;
+DROP TABLE TravelAgency;
+DROP TABLE Date;
+DROP TABLE Time;
+DROP TABLE LocalAirport;
+DROP TABLE DestinationAirport;
+DROP TABLE Airline;
+DROP TABLE Hotel;
---
+++ dw/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO TravelAgency
+VALUES (1, 'sample name', 'gdansk', 'poland');
+
+INSERT INTO Employee
+VALUES (1, 1, '00291934559', 'Jan Kowalski', 'PhD', 'up to one year', 1);
+
+INSERT INTO Date
+VALUES (1, 12, 2021, 'September', 9);
+
+INSERT INTO Time
+VALUES (1, 2, 43);
+
+INSERT INTO LocalAirport
+VALUES (1, 'Katowice Pyrzowice Airport', 'Pyrzowice', 'Poland');
+
+INSERT INTO DestinationAirport
+VALUES (1, 'Luton Airport', 'Luton', 'United Kingdom');
+
+INSERT INTO Airline
+VALUES (1, 'Ryanair');
+
+INSERT INTO Hotel
+VALUES (1, 'Sample Hotel', 'Essex', 'United Kingdom', 0);
+
+INSERT INTO Flights
+VALUES (1, 1, 1, 1, 1, 1, 1, 163, 200, 0.1);
+
+INSERT INTO AirportNearHotel
+VALUES (1, 1, 92, 70);
+
+INSERT INTO TripScheduling
+VALUES (1, 1, 1, 1, 1, 1, 1, 1, 1, 300, 700, 200, 0);
